8d2a1f70af25cfcaced1165dd0a76a809fcdd187
Jessica Tracy <jessicatracy@Jessicas-MacBook-Pro.local>
2016-08-18 11:06:56 -0400

    Overdoing commits a bit here

---
+++ src/tiyinc/noobs/Day9Runner.java
@@ -0,0 +1,16 @@
+package tiyinc.noobs;
+
+/**
+ * Created by jessicatracy on 8/18/16.
+ */
+public class Day9Runner {
+    public static void main(String[] args) {
+        System.out.println("Running....");
+        Day9Runner myRunner = new Day9Runner();
+        myRunner.testWriteFile();
+    }
+
+    public void testWriteFile() {
+        System.out.println("testWriteFile()");
+    }
+}
