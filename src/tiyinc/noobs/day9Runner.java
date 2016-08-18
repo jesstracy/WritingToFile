1299fb7ec0738cf05295538ecf145ac9d109da18
Jessica Tracy <jessicatracy@Jessicas-MacBook-Pro.local>
2016-08-18 11:03:43 -0400

    Base version of day9Runner

---
+++ src/tiyinc/noobs/day9Runner.java
@@ -0,0 +1,10 @@
+package tiyinc.noobs;
+
+/**
+ * Created by jessicatracy on 8/18/16.
+ */
+public class day9Runner {
+    public static void main(String[] args) {
+        System.out.println("Running....");
+    }
+}
